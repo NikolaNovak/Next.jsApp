e2f62777610d6e5dc194447b4993daff48ddcbc5
Nikola Novak <nikola.novak.hr@gmail.com>
2020-07-18 20:12:13 +0200

    Using the getInitialProps lifecycle hook

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -2,11 +2,21 @@ import React from "react";
 
 import User from "../../components/User";
 
-const authIndexPage = () => (
+const authIndexPage = (props) => (
   <div>
-    <h1>The auth index page</h1>
+    <h1>The auth index page - {props.appName}</h1>
     <User name="Someone" age="22" />
   </div>
 );
+
+authIndexPage.getInitialProps = (context) => {
+  console.log(context);
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ appName: "Super App (auth)" });
+    }, 1000);
+  });
+  return promise;
+};
 
 export default authIndexPage;
